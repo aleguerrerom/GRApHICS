e2877a687323acdfae3c97cb6796c7fdd2d7cb96
Alejandro <Aleguemad94@yahoo.es>
2021-04-20 01:18:38 +0000

    TEST

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,47 @@
 import React from "react";
-
+import * as CryptoCharts from "cryptocharts";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import TradingViewWidget, { Themes } from "react-tradingview-widget";
 
 //create your first component
 export function Home() {
+	CryptoCharts.roiComparison({
+		chart_id: "mychart",
+		cryptocompare_tickers: ["BTC", "ETH"],
+		iconomi_tickers: ["BLX", "CAR"],
+		last_days: 90
+	});
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="row">
+				<div className="col">
+					<TradingViewWidget
+						symbol="BTCUSD"
+						theme={Themes.DARK}
+						locale="es"
+						autosize
+					/>
+				</div>
+				<div className="col">
+					<TradingViewWidget
+						symbol="ETHE"
+						theme={Themes.DARK}
+						locale="es"
+						autosize
+					/>
+				</div>
+				<div className="col">
+					<TradingViewWidget
+						symbol="XRPUSD"
+						theme={Themes.DARK}
+						locale="es"
+						autosize
+					/>
+				</div>
+			</div>
+
+			<div id="mychart"></div>
 		</div>
 	);
 }
